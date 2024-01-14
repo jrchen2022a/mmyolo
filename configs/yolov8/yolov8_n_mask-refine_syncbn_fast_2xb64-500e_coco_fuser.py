0ac2794f1177bc25219eb0d057dc2b6db8b51e09
jrchen2022a <jrchen3106@163.com>
2024-01-14 20:41:53 +0800

    补充fuser_mr的n.s

---
+++ configs/yolov8/yolov8_n_mask-refine_syncbn_fast_2xb64-500e_coco_fuser.py
@@ -0,0 +1,15 @@
+_base_ = './yolov8_n_mask-refine_syncbn_fast_8xb16-500e_coco.py'
+
+train_batch_size_per_gpu = 64
+train_dataloader = dict(
+    batch_size=train_batch_size_per_gpu)
+optim_wrapper = dict(
+    optimizer=dict(
+        batch_size_per_gpu=train_batch_size_per_gpu))
+
+work_dir = _base_.work_dir_root+'/work_dirs/coco/yolov8_n_mask-refine_syncbn_fast_2xb64-500e_coco_fuser/'
+visualizer = dict(
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+        dict(type='WandbVisBackend', init_kwargs=dict(project='research_coco', name='yolov8_n_fuser_mr'))
+    ])
