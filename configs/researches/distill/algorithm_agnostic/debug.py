34f54cb9f2cbd37b326fdfbf7460cda1f1a86428
jrchen2022a <jrchen3106@163.com>
2024-01-23 13:23:08 +0800

    mgd-neck:v8-n-fuser->v8-n-dscv2 实验

--- configs/researches/distill/algorithm_agnostic/debug.py
+++ configs/researches/distill/algorithm_agnostic/debug.py
@@ -1,5 +1,6 @@
 # 当你想调试时，_base_引入这个文件，并更改如下_base_
-_base_ = '../../../yolov8/yolov8_n_mask-refine_syncbn_fast_8xb16-500e_coco.py'
+_base_ = ['../algorithm_agnostic/distill-dw_schedule_8xb16_200e_dw.py',
+          '../algorithm_agnostic/distill-neck_yolov8-n-fuser_yolov8-n-dscv2.py']
 
 train_batch_size_per_gpu = 2
 _base_.train_dataloader.batch_size=train_batch_size_per_gpu
