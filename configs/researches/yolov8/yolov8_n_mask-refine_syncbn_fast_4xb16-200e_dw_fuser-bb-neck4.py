039deeb39ef0809ad85a4e11b45cdf0085db7ce8
jrchen2022a <jrchen3106@163.com>
2024-03-13 10:32:55 +0800

    消融实验补充

---
+++ configs/researches/yolov8/yolov8_n_mask-refine_syncbn_fast_4xb16-200e_dw_fuser-bb-neck4.py
@@ -0,0 +1,18 @@
+_base_ = './yolov8_n_mask-refine_syncbn_fast_4xb16-200e_dw.py'
+
+work_dir = _base_.work_dir_root+'{{fileBasenameNoExtension}}/'
+_base_.visualizer.vis_backends[1].init_kwargs.name='{{fileBasenameNoExtension}}'
+# work_dir = _base_.work_dir_root+'/work_dirs/temp/'
+# visualizer = dict(
+#     vis_backends=[
+#         dict(type='LocalVisBackend')
+#     ])
+
+model = dict(
+    backbone=dict(
+            type='YOLOv8SelectorCSPDarknet',
+            num_selectors=4,
+            selector_type='SelectorCSPLayerWithTwoConv'),
+    neck=dict(
+        type='YOLOv8SelectorPAFPN',
+        num_selectors=4))
