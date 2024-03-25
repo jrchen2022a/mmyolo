bdf266f420719959025b50291041e6b1a1478363
= <jrchen3106@163.com>
2024-03-25 18:26:35 +0800

    修改蒸馏epoch为200 + 新增sckd:yolo-fa->yolo-lite

---
+++ configs/researches/distill/algorithm_agnostic/distill-dw_schedule_8xb16_200e_dw_mask-refine.py
@@ -0,0 +1,13 @@
+_base_ = '../../_base_/dw_schedule_v8_8xb16_500e_mask-refine.py'
+
+wandb_project_name = 'distill_exp_v3'
+_base_.visualizer.vis_backends[1].init_kwargs.project = wandb_project_name
+work_dir_root = _base_.save_work_dir_root + wandb_project_name + '/'
+
+max_epochs = 200
+_base_.default_hooks.param_scheduler.max_epochs = max_epochs
+_base_.custom_hooks[1].switch_epoch = max_epochs - _base_.close_mosaic_epochs
+_base_.train_cfg.max_epochs = max_epochs
+_base_.train_cfg.dynamic_intervals = [((max_epochs - _base_.close_mosaic_epochs), _base_.val_interval_stage2)]
+
+val_cfg = dict(type='mmrazor.SingleTeacherDistillValLoop')
--- configs/researches/distill/cwd/cwd-neck_yolov8-n-fuser_yolov8-n-lite_mask-refine_syncbn_fast_4xb16_200e_dw.py
+++ configs/researches/distill/cwd/cwd-neck_yolov8-n-fuser_yolov8-n-lite_mask-refine_syncbn_fast_4xb16_200e_dw.py
@@ -1,4 +1,4 @@
-_base_ = ['../algorithm_agnostic/distill-dw_schedule_8xb16_100e_dw_mask-refine.py',
+_base_ = ['../algorithm_agnostic/distill-dw_schedule_8xb16_200e_dw_mask-refine.py',
           '../algorithm_agnostic/distill-neck_yolov8-n-fuser_yolov8-n-lite.py']
 
 work_dir = _base_.work_dir_root+'{{fileBasenameNoExtension}}/'
@@ -10,5 +10,6 @@ model = dict(
             loss_s2=dict(type='ChannelWiseDivergence', tau=4, loss_weight=10),
             loss_s3=dict(type='ChannelWiseDivergence', tau=4, loss_weight=10),
             loss_s4=dict(type='ChannelWiseDivergence', tau=4, loss_weight=10))))
-
-#_base_.custom_hooks.append(dict(type='DistillWeightHook', _scope_='mmrazor'))
+del _base_.custom_hooks[-1]
+_base_.custom_hooks.append(dict(type='DistillWeightHook', _scope_='mmrazor', start_epoch=50, stop_epoch=150))
+_base_.custom_hooks.append(dict(type='StopDistillHook', _scope_='mmrazor', stop_epoch=80))
