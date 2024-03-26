7828dba23db401a581d91dd59354bcce4c62e337
= <jrchen3106@163.com>
2024-03-26 10:38:42 +0800

    调参sckd

--- configs/debug.py
+++ configs/debug.py
@@ -1,4 +1,4 @@
-_base_ = 'researches/distill/cwd/cwd-neck_yolov8-n-fuser_yolov8-n-lite_mask-refine_syncbn_fast_4xb16_100e_dw.py'
+_base_ = 'researches/distill/spatialchannelkd/sckd-bb_yolov8-n-fuser_yolov8-n-lite_mask-refine_syncbn_fast_4xb16_200e_dw.py'
 
 _base_.train_dataloader.batch_size = 2
 _base_.optim_wrapper.optimizer.batch_size_per_gpu = 2
@@ -6,3 +6,4 @@ _base_.train_dataloader.num_workers = 1
 
 work_dir = 'work_dirs/temp/'
 del _base_.visualizer.vis_backends[1]
+
--- configs/researches/distill/spatialchannelkd/sckd-bb_yolov8-n-fuser_yolov8-n-lite_mask-refine_syncbn_fast_4xb16_200e_dw.py
+++ configs/researches/distill/spatialchannelkd/sckd-bb_yolov8-n-fuser_yolov8-n-lite_mask-refine_syncbn_fast_4xb16_200e_dw.py
@@ -7,10 +7,10 @@ _base_.visualizer.vis_backends[1].init_kwargs.name = '{{fileBasenameNoExtension}
 model = dict(
     distiller=dict(
         distill_losses=dict(
-            loss_s2=dict(type='SpatialChannelWiseDivergence', tau=1, loss_weight=10),
-            loss_s3=dict(type='SpatialChannelWiseDivergence', tau=1, loss_weight=10),
-            loss_s4=dict(type='SpatialChannelWiseDivergence', tau=1, loss_weight=10))))
+            loss_s2=dict(type='SpatialChannelWiseDivergence', tau=3, loss_weight=16),
+            loss_s3=dict(type='SpatialChannelWiseDivergence', tau=3, loss_weight=16),
+            loss_s4=dict(type='SpatialChannelWiseDivergence', tau=3, loss_weight=16))))
 
 del _base_.custom_hooks[-1]
-# _base_.custom_hooks.append(dict(type='DistillWeightHook', _scope_='mmrazor', start_epoch=80, stop_epoch=160))
-_base_.custom_hooks.append(dict(type='StopDistillHook', _scope_='mmrazor', stop_epoch=50))
+_base_.custom_hooks.append(dict(type='DistillWeightHook', _scope_='mmrazor', start_epoch=0, stop_epoch=200))
+# _base_.custom_hooks.append(dict(type='StopDistillHook', _scope_='mmrazor', stop_epoch=50))
